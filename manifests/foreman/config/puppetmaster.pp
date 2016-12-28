0690ef632f7f32f0288d3dffdb393590a249f7cd
andreias <andrei.asan@cegeka.com>
2016-12-28 11:40:40 +0100

    [PUPPET] move puppetmaster foreman.rb to hiera-data 4_role/puppetmaster + fix puppetmaster.pp requires

--- manifests/foreman/config/puppetmaster.pp
+++ manifests/foreman/config/puppetmaster.pp
@@ -28,34 +28,20 @@ define puppet::foreman::config::puppetmaster(
     content => template("${module_name}/foreman/etc/foreman.yaml.erb")
   }
 
-  case $operatingsystemmajrelease {
-    6: { $report_location = '/usr/lib/ruby/site_ruby/1.8/puppet/reports' }
-    7: { $report_location = '/usr/share/ruby/vendor_ruby/puppet/reports/' }
-  }
-
-  file { "${report_location}/foreman.rb":
-    ensure  => file,
-    owner   => $puppet_user,
-    group   => $puppet_user,
-    mode    => '0644',
-    source  => "puppet:///modules/${module_name}/foreman.rb",
-    notify  => Service['puppet']
-  }
-
   file { '/usr/local/scripts/push_facts_to_foreman.rb':
     ensure  => file,
     owner   => $puppet_user,
     group   => $puppet_user,
     mode    => '0755',
-    source  => "puppet:///modules/${module_name}/push_facts_to_foreman.rb"
+    source  => "puppet:///modules/${module_name}/push_facts_to_foreman.rb",
+    require => File['/usr/local/scripts']
   }
 
   cron { 'push_facts_to_foreman':
     command => 'sudo -u puppet /usr/local/scripts/push_facts_to_foreman.rb --push-facts-parallel',
     user    => root,
     hour    => 2,
-    minute  => 0,
-    require => [ File['/usr/local/scripts/push_facts_to_foreman.rb'], File['/usr/local/scripts'] ]
+    minute  => 0
   }
 
 }
